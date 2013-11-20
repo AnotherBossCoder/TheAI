dde9fc94d90d319a558b1af6a013e4500706f975
ethanarc <ethanprintz@gmail.com>
2013-11-20 11:48:29 -0800

    Update TheAI.java

--- TheAI/TheAI/src/TheAI.java
+++ TheAI/TheAI/src/TheAI.java
@@ -5,7 +5,9 @@ public class TheAI {
 	public static void main(String[] args) {
 	Scanner scan = new Scanner(system.in);
 	Random random = new Random();
-
+//_______________________________________________________________________________________________________________________
+	//Variable Creation Section
+	int hello = random.nextInt(4);
 	}
 
 }
