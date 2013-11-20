f9b0c8bfa816a8217f0ae2519c76974c2f45a47f
ethanarc <ethanprintz@gmail.com>
2013-11-20 12:10:22 -0800

    Update TheAI.java

--- TheAI/TheAI/src/TheAI.java
+++ TheAI/TheAI/src/TheAI.java
@@ -9,6 +9,7 @@ public class TheAI {
 	//Variable Creation Section
 	int hello = random.nextInt(4);
 	String name = ("Master");
+	int again;//This variable will preform a lot like a boolean.
 //_______________________________________________________________________________________________________________________
 	//Main Line
 	System.out.println("Hello there human i'm "+name);
@@ -17,8 +18,20 @@ public class TheAI {
 	//Response
 	String main;
 	main = scan.nextLine();
+	//_______________________________________________________________________________________________________________
+		//Response to "Hello"
 		if(main.equals("hello")||(main.equals("Hello")){
-			
+			if(hello==1){
+				System.out.println("Well hello to you too, underling");
+			}
+			else if(hello==2){
+				
+			}
+			else if(hello==3){
+				
+			}
+	//_______________________________________________________________________________________________________________
+		//Response to 
 		}
 	}
 
