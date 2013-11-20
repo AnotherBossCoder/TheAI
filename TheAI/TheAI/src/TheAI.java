d4b923cd39a0bae423fba09c9adb154dd790da61
ethanarc <ethanprintz@gmail.com>
2013-11-20 11:52:52 -0800

    Update TheAI.java

--- TheAI/TheAI/src/TheAI.java
+++ TheAI/TheAI/src/TheAI.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class TheAI {
 
 	public static void main(String[] args) {
-	Scanner scan = new Scanner(system.in);
+	Scanner scan = new Scanner(System.in);
 	Random random = new Random();
 //_______________________________________________________________________________________________________________________
 	//Variable Creation Section
